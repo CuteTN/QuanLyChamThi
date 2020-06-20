a9d4d052821d12642b3c97ebce56f52cf5c923d9
UIT-SE-TrungVo <trungvo482@gmail.com>
2020-06-20 15:43:29 +0700

    Thêm màn hình đăng nhập, màn hình thông báo popup

---
+++ QuanLyChamThi/View/Animation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyChamThi.View
+{
+    static class Animation
+    {
+        public static bool Message(Window sender, string message, string detail, float stayTime)
+        {
+            WindowNotification windowNotification = new WindowNotification(message, detail, stayTime);
+            return (windowNotification.ShowDialog() == true);
+        } 
+    }
+}
---
+++ QuanLyChamThi/View/WindowLogin.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyChamThi.View
+{
+    /// <summary>
+    /// Interaction logic for WindowLogin.xaml
+    /// </summary>
+    public partial class WindowLogin : Window
+    {
+
+        public WindowLogin()
+        {
+            InitializeComponent();
+            /** CREATE GRID COLUMNS AND ROWS **/
+            for (int i = 0; i < 8; i++)
+                mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            for (int i = 0; i < 8; i++)
+                mainGrid.RowDefinitions.Add(new RowDefinition());
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (Animation.Message(this, "Đăng nhập thành công", "Chào mừng abcxyz", 1) == false)
+                this.Close();
+        }
+    }
+}
