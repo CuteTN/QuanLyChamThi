94c54b6c80169e8522a92b7560821e10523df257
CuteTN <quantiennghia001@gmail.com>
2020-06-28 13:35:57 +0700

    Add Save report as file feature

---
+++ QuanLyChamThi/Utilities/ExcelExporter.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace QuanLyChamThi.Utilities
+{
+    static class ExcelExporter
+    {
+        public static void Export(List<List<string>> table, string filePath)
+        {
+            if(table == null)
+                return;
+
+            int NumberOfRows = table.Count;
+            int NumberOfCols = NumberOfRows>0? table[0].Count : 0;
+
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+            Excel.Range rangeToHoldHyperlink;
+            Excel.Range CellInstance;
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            xlApp.DisplayAlerts = false;
+            //Dummy initialisation to prevent errors.
+            rangeToHoldHyperlink = xlWorkSheet.get_Range("A1", Type.Missing);
+            CellInstance = xlWorkSheet.get_Range("A1", Type.Missing);
+
+            for (int i = 0; i < NumberOfRows; i++)
+            {
+                for (int j = 0; j < NumberOfCols; j++)
+                {
+                    string cellContent = table[i][j];
+                    xlWorkSheet.Cells[i + 1, j + 1] = cellContent;
+                }
+            }
+
+            xlWorkBook.SaveAs(filePath);
+            xlWorkBook.Close();
+        }
+    }
+}
--- QuanLyChamThi/ViewModel/YearlyReportViewModel.cs
+++ QuanLyChamThi/ViewModel/YearlyReportViewModel.cs
@@ -1,4 +1,5 @@
-﻿using QuanLyChamThi.Command;
+﻿using Microsoft.Office.Interop.Excel;
+using QuanLyChamThi.Command;
 using QuanLyChamThi.Model;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Forms;
 using System.Windows.Input;
 
 namespace QuanLyChamThi.ViewModel
@@ -166,6 +168,76 @@ namespace QuanLyChamThi.ViewModel
             model.UpdateFromDB();
             ListReport = model.Data;
         }
+        #endregion
+
+        #region button Save As File
+        // this is for exporting to excel
+        private List<List<string>> createReportTable(ObservableCollection<SubjectYearlyReportModel> listReport)
+        {
+            // BADCODE
+            List<List<string>> result = new List<List<string>>();
+
+            // Index - Subject - NOTest - NOTestResult - RTest - RTestsult
+            
+            // header
+            result.Add(new List<string>());
+            string[] headers = "STT/Tên môn học/Số lượng đề thi/Số lượng bài chấm/Tỉ lệ đề thi/Tỉ lệ bài chấm".Split('/');
+            result[0].AddRange(headers);
+
+            // content
+            foreach(var report in listReport)
+            {
+                result.Add(new List<string>());
+
+                result.Last().Add(report.Index.ToString());
+                result.Last().Add(report.Subject);
+                result.Last().Add(report.TestCount.ToString());
+                result.Last().Add(report.TestResultCount.ToString());
+                result.Last().Add(report.TestRatio.ToString());
+                result.Last().Add(report.TestResultRatio.ToString());
+            }
+
+            return result;
+        }
+        
+        private void SaveAsFileFunction()
+        {
+            var dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.DefaultExt = ".xlsx";
+            dlg.Filter = "Microsoft Office Excel documents|*.xlsx";
+            bool? userAcceptSaving = dlg.ShowDialog();
+
+            if (userAcceptSaving==null || !userAcceptSaving.Value)
+                return;
+
+            if (! dlg.ValidateNames )
+            {
+                // MORECODE
+                return;
+            }
+
+            string fileName = dlg.FileName;
+
+            var dataToSave = createReportTable(ListReport);
+            QuanLyChamThi.Utilities.ExcelExporter.Export(dataToSave, fileName);
+        }
+
+        private ICommand _saveAsFileCommand = null;
+        public ICommand SaveAsFileCommand
+        {
+            get
+            {
+                if (_saveAsFileCommand == null)
+                    _saveAsFileCommand = new RelayCommand(param => SaveAsFileFunction(), null);
+                return _saveAsFileCommand;
+            }
+            set
+            {
+                _saveAsFileCommand = value;
+                OnPropertyChange("SaveAsFileCommand");
+            }
+        }
+
         #endregion
 
         #region Internal business logic
